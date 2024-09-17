5109f7bf4062b86cf16ae9d89207a3858906e451
Jhoanna May Lacorte <152822639+JhoannaMay@users.noreply.github.com>
2024-09-17 10:35:01 +0800

    Update AttendanceSystemAdapter.java

--- AttendanceSystemAdapter.java
+++ AttendanceSystemAdapter.java
@@ -1,10 +1,12 @@
-public class AttendanceSystemAdapter implements SystemInt {
+public class AttendanceSystemAdapter implements SchoolManagementApp {
     private AttendanceSystem attendanceSystem;
 
-    public AttendanceSystemAdapter (AttendanceSystem attendanceSystem) {
+    public AttendanceSystemAdapter(AttendanceSystem attendanceSystem) {
         this.attendanceSystem = attendanceSystem;
     }
-    public String integrateSystem() {
-        return attendanceSystem.markAttendance();
+
+    @Override
+    public void integrateSystem() {
+        attendanceSystem.markAttendance();
     }
 }
